1447a996583c546e222669c79dfc16d7664aa9fa
Dane Maison <danemaison@gmail.com>
2019-10-13 23:30:28 -0700

    created messageList component

---
+++ client/src/components/messagelist.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class MessageList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {
+    // fetch messages
+  }
+  render() {
+    return (
+      <div className="messages">
+
+      </div>
+    );
+  }
+}
+
+export default MessageList;
